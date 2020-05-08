9047dda9faec856814a419160bc314a5b577f45a
wangtingyun <wangtingyun@aipai.com>
2020-05-08 11:35:55 +0800

    feat: 添加 RxPy Demo

--- demo/rx_v1_demo.py
+++ demo/rx_v1_demo.py
@@ -3,13 +3,14 @@
 # @Time     : 2020/04/28
 
 import sys
-from rx import Observable, Observer
 
 
 def get_input_demo():
     """
     从输入框获取输入，从第 10 次输入开始取前5次的输入，打印出来
     """
+    from rx import Observable
+
     def get_input(observer):
         count = 0
         while True:
@@ -21,20 +22,10 @@ def get_input_demo():
                 break
             observer.on_next(line)
 
-    method = 1
-    if method == 1:
-        # 方式一
-        Observable.create(get_input) \
-            .skip(10) \
-            .take(5) \
-            .map(lambda line: "接收的数据为：{}".format(line)) \
-            .subscribe(print)
-    elif method == 2:
-        # 方式二
-        Observable.create(get_input) \
-            .skip(10) \
-            .take(5) \
-            .subscribe(lambda line: print("接收的数据为：{}".format(line)))
+    Observable.create(get_input) \
+        .skip(5) \
+        .take(10) \
+        .subscribe(lambda line: print("接收的数据为：{}".format(line)))
 
 
 def test_create_operator():
@@ -104,7 +95,7 @@ def simplify_subscribe():
     """
     简化 subscribe 订阅函数
     """
-    from rx import Observable, Observer
+    from rx import Observable
 
     # 创建 Observable 对象
     source = Observable.from_(["Alpha", "Beta", "Gamma", "Delta", "Epsilon"])
@@ -117,25 +108,137 @@ def simplify_subscribe():
 
 
 def test_map_and_filter():
-    from rx import Observable, Observer
+    from rx import Observable
 
-    # 创建 Observable 对象
-    # source = Observable.from_(["Alpha", "Beta", "Gamma", "Delta", "Epsilon"])
+    chaining = True
 
-    # 对数据项进行映射
-    # lengths = source.map(lambda s: len(s))
+    if chaining:
+        Observable.from_(["Alpha", "Beta", "Gamma", "Delta", "Epsilon"]) \
+            .map(lambda s: len(s)) \
+            .filter(lambda i: i >= 5) \
+            .subscribe(lambda value: print("Received {0}".format(value)))
+    else:
+        # 创建 Observable 对象
+        source = Observable.from_(["Alpha", "Beta", "Gamma", "Delta", "Epsilon"])
 
-    # 过滤数据项
-    # filterd = lengths.filter(lambda i: i >= 5)
+        # 对数据项进行映射
+        lengths = source.map(lambda s: len(s))
 
-    # 开始订阅观察者
-    # filterd.subscribe(on_next=lambda value: print("Received {0}".format(value)))
+        # 过滤数据项
+        filterd = lengths.filter(lambda i: i >= 5)
 
-    Observable.from_(["Alpha", "Beta", "Gamma", "Delta", "Epsilon"]) \
-        .map(lambda s: len(s)) \
-        .filter(lambda i: i >= 5) \
-        .subscribe(lambda value: print("Received {0}".format(value)))
+        # 开始订阅观察者
+        filterd.subscribe(on_next=lambda value: print("Received {0}".format(value)))
+
+
+def test_interval():
+    from rx import Observable
+
+    Observable.interval(1000) \
+        .map(lambda i: "收到了数据 {0}".format(i)) \
+        .subscribe(lambda s: print(s))
+
+    input("---- 按任意退出 -----\n")
+
+
+def test_multicasting():
+    from rx import Observable
+    from random import randint
+
+    def no_connected():
+        emission_ints = Observable.range(1, 3).map(lambda i: randint(1, 100))
+        emission_ints.subscribe(lambda data: print("subscribe_1 收到数据：{0}".format(data)))
+        emission_ints.subscribe(lambda data: print("subscribe_2 收到数据：{0}".format(data)))
+
+    def connected():
+        emission_ints = Observable.range(1, 3).map(lambda i: randint(1, 100)).publish()
+        emission_ints.subscribe(lambda data: print("subscribe_1 收到数据：{0}".format(data)))
+        emission_ints.subscribe(lambda data: print("subscribe_2 收到数据：{0}".format(data)))
+        emission_ints.connect()
+
+    def auto_connected():
+        emission_ints = Observable.range(1, 3).map(lambda i: randint(1, 100)).publish().auto_connect(2)
+        emission_ints.subscribe(lambda data: print("subscribe_1 收到数据：{0}".format(data)))
+        emission_ints.subscribe(lambda data: print("subscribe_2 收到数据：{0}".format(data)))
+
+    auto_connected()
+
+
+def test_zip():
+    import operator
+    from rx import Observable
+
+    a = Observable.of(1, 2, 3, 4, 5)
+    b = Observable.of(1, 2, 3, 4, 5)
+
+    a.zip(b, lambda i, j: operator.mul(i, j)) \
+        .subscribe(lambda data: print("Received data: {0}".format(data)))
+
+
+def test_combine_observable():
+    from rx import Observable
+
+    word = Observable.from_(["Alpha", "Beta", "Gamma", "Delta", "Epsilon"])
+    interval = Observable.interval(1000)
+
+    Observable.zip(word, interval, lambda w, i: (w, i)) \
+        .subscribe(lambda data: print(data))
+
+    input("---- 按任意退出 -----\n")
+
+
+def test_concurrency():
+    import time
+    import multiprocessing
+    from threading import current_thread
+
+    from rx import Observable
+    from rx.concurrency import ThreadPoolScheduler
+
+    # 根据 CPU 的核心个数创建线程池调度器
+    optimal_thread_count = multiprocessing.cpu_count()
+    pool_scheduler = ThreadPoolScheduler(optimal_thread_count)
+
+    # 通过休眠来模拟长时间运算
+    def intense_calculation(value):
+        time.sleep(1)
+        print("Observable run in: {0}".format(current_thread().name))
+        return value
+
+    Observable.range(1, 3) \
+        .map(lambda i: intense_calculation(i)) \
+        .subscribe_on(pool_scheduler) \
+        .subscribe(on_next=lambda i: print("subscriber run in: {0} data: {1}".format(current_thread().name, i)))
+
+
+def test_python_alignment():
+    from rx import Observable
+
+    def concat():
+        xs = Observable.from_([0, 1, 2, 3, 4, 5, 6])
+        ys = Observable.from_([7, 8, 9])
+        zs = xs + ys
+        zs.subscribe(lambda data: print("data: {0}".format(data)))
+
+    def duplicate():
+        xs = Observable.from_([0, 1])
+        ys = xs * 3
+        ys.subscribe(lambda data: print("data: {0}".format(data)))
+
+    def slice():
+        xs = Observable.from_([0, 1, 2, 3, 4, 5, 6])
+        ys = xs[2:6]
+        ys.subscribe(lambda data: print("data: {0}".format(data)))
+
+    slice()
+
+
+def test_blocking():
+    from rx import Observable
+
+    res = Observable.of(1, 2, 3, 4, 6).to_blocking().last()
+    print("Received last data: {0}".format(res))
 
 
 if __name__ == '__main__':
-    test_map_and_filter()
+    test_concurrency()
--- demo/rx_v3_demo.py
+++ demo/rx_v3_demo.py
@@ -1,39 +1,105 @@
 # -*- coding: utf-8 -*-
 # @Author   : wangtingyun
-# @Time     : 2020/04/28
-
-import sys
-from rx import of, create
+# @Time     : 2020/05/06
 
 
-def push_five_strings(observer, scheduler):
-    observer.on_next("Alpha")
-    observer.on_next("Beta")
-    observer.on_next("Gamma")
-    observer.on_next("Delta")
-    observer.on_next("Esilon")
-    observer.on_completed()
+def test_create():
+    from rx import create
+
+    def push_strings(observer, scheduler):
+        observer.on_next("Alpha")
+        observer.on_next("Beta")
+
+    source = create(push_strings)
+
+    source.subscribe(lambda i: print("Received data: {0}".format(i)))
 
 
-def get_input(observer):
-    while True:
-        line = sys.stdin.readline()
-        if line == '\n':
-            observer.on_completed()
-            break
-        observer.on_next(line)
+def test_of():
+    from rx import of, from_
+
+    # source = of(*[1, "Alpha", [2, 3]])
+    source = of(1, "Alpha", [2, 3])
+
+    source.subscribe(lambda i: print("Received data: {0}".format(i)))
 
 
-if __name__ == '__main__':
-    # 通过 create 创建 Observable
-    # observable = create(push_five_strings)
+def test_pipe():
+    import rx
+    from rx import operators
 
-    # 通过 of 创建 Observable
-    observable = of("Alpha", "Beta", "Gamma", "Delta", "Esilon")
+    rx.of("Alpha", "Beta", "GammaRay").pipe(
+        operators.map(lambda s: len(s)),
+        operators.filter(lambda i: i >= 5)
+    ).subscribe(lambda data: print("Received data: {0}".format(data)))
 
-    # 启动
-    observable.subscribe(
-        on_next=lambda i: print("Received {0}".format(i)),
-        on_error=lambda e: print("Error occured {0}".format(e)),
-        on_completed=lambda: print("Done!")
-    )
+
+def test_pipe_function():
+    import rx
+    from rx import operators
+
+    def filter_string_length():
+        return rx.pipe(
+            operators.map(lambda s: len(s)),
+            operators.filter(lambda i: i >= 5)
+        )
+
+    rx.of("Alpha", "Beta", "GammaRay").pipe(
+        filter_string_length()
+    ).subscribe(lambda data: print("Received data: {0}".format(data)))
+
+
+def test_zip():
+    import operator
+    import rx
+    from rx import operators
+
+    a = rx.of(1, 2, 3)
+    b = rx.of(1, 2, 3)
+
+    a.pipe(
+        operators.zip(b),   # 返回一个由 a 和 b 数据项组成的元组
+        operators.map(lambda z: operator.mul(z[0], z[1]))
+        # operators.starmap(operator.mul)
+    ).subscribe(lambda data: print("Received data: {0}".format(data)))
+
+
+def test_merge():
+    import rx
+
+    obs1 = rx.of(1, 2)
+    obs2 = rx.of(3, 4)
+
+    res = rx.merge(obs1, obs2)
+    res.subscribe(lambda data: print("Received data: {0}".format(data)))
+
+
+def test_custom_operator():
+    import rx
+
+    def lowercase():
+        # source 为传进来 Observable
+        def observable_handle(source):
+            def subscribe(observer, scheduler=None):
+                # 重写 on_next 来达到处理数据项的目的
+                def on_next(value):
+                    observer.on_next(value.lower())
+
+                return source.subscribe(
+                    on_next,    # 替换为自己的 on_next
+                    observer.on_error,
+                    observer.on_completed,
+                    scheduler
+                )
+            # 返回一个自定义的新的 Observable
+            return rx.create(subscribe)
+
+        return observable_handle
+
+    rx.of("Alpha", "Beta", "Gamma", "Delta", "Epsilon").pipe(
+        lowercase()
+    ).subscribe(lambda data: print("Received data: {0}".format(data)))
+
+
+if __name__ == "__main__":
+    test_create()
